b70f6a3433bd40d2e2837142ab3e8c7ca1a31bc8
Chetanchaudhary11 <cc181972@gmail.com>
2023-12-27 13:22:32 +0530

    Assignment Advance-Ui Design Flutter

---
+++ Flutter - Advance UI Designing Assignment/Phone Call Permission/lib/phone_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class PhoneCallScreen extends StatefulWidget {
+  const PhoneCallScreen({super.key});
+
+  @override
+  State<PhoneCallScreen> createState() => _PhoneCallScreenState();
+}
+
+class _PhoneCallScreenState extends State<PhoneCallScreen> {
+
+  void _checkPermission() async {
+    PermissionStatus status = await Permission.phone.request();
+
+    if (status == PermissionStatus.granted) {
+      Fluttertoast.showToast(msg: 'Permission granted');
+    } else {
+      Fluttertoast.showToast(msg: 'Permission denied');
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    _checkPermission();
+  }
+
+  @override
+  Widget build(BuildContext context) {   
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Phone Call App"),
+        backgroundColor: Colors.green,
+      ),
+      body: const Center(
+        child: Text("Phone Numbers"),
+      ),
+    );
+  }
+}
