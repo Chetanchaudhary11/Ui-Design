314993bbe9c41f3083da8a90924a928afc028429
Chetanchaudhary11 <cc181972@gmail.com>
2024-02-07 18:25:17 +0530

    Assignment intro flutter

---
+++ Assignment-Module1/circleArea.dart
@@ -0,0 +1,9 @@
+import 'dart:math';
+
+void main(){
+  double radius = 5.0;
+ //Circle Area
+  double area = pi * radius * radius;
+
+  print("Area of Circle: $area");
+}
---
+++ Assignment-Module1/information.dart
@@ -0,0 +1,11 @@
+void main(){
+  String name = "Chetan Chaudhary";
+  String DOB = "/08/99";
+  int age = 24;
+  String address = "Akhbar nagar, Ahmedabad";
+
+  print("Name: $name");
+  print("DOB: $DOB");
+  print("$age");
+  print("$address");
+}
